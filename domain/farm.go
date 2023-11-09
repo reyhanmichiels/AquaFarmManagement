52c29954187ebf923596f53c700e2278111ee1a8
reyhanmichiels <michiels.reyhan@gmail.com>
2023-11-10 01:04:03 +0700

    feat(createFarm): create struct for bind request and create hook for before create

--- domain/farm.go
+++ domain/farm.go
@@ -1,6 +1,11 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 // Model for Farm entity
 type Farm struct {
@@ -9,4 +14,14 @@ type Farm struct {
 	Name      string    `json:"name" gorm:"type:varchar(100); not null; unique"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+// Automate generate uuid when create farm
+func (farm *Farm) BeforeCreate(tx *gorm.DB) error {
+	farm.ID = uuid.NewString()
+	return nil
+}
+
+type CreateFarmBind struct {
+	Name string `json:"name" binding:"required,max=100,min=4"`
 }
