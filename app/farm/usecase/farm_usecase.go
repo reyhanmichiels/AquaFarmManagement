8612f2bb4c634759e6f62516c515eb5e5864f966
reyhanmichiels <michiels.reyhan@gmail.com>
2023-11-10 11:56:19 +0700

    refactor(updateFarm): change CreateFarmBind to FarmBind

--- app/farm/usecase/farm_usecase.go
+++ app/farm/usecase/farm_usecase.go
@@ -10,7 +10,7 @@ import (
 )
 
 type IFarmUsecase interface {
-	Create(request domain.CreateFarmBind) (domain.Farm, any)
+	Create(request domain.FarmBind) (domain.Farm, any)
 }
 
 type FarmUsecase struct {
@@ -23,7 +23,7 @@ func NewFarmUsecase(farmRepository repository.IFarmRepository) IFarmUsecase {
 	}
 }
 
-func (farmUsecase *FarmUsecase) Create(request domain.CreateFarmBind) (domain.Farm, any) {
+func (farmUsecase *FarmUsecase) Create(request domain.FarmBind) (domain.Farm, any) {
 	// check for duplicate entry
 	isFarmExist := farmUsecase.farmRepository.FindFarmByCondition(&domain.Farm{}, "name = ?", request.Name)
 	if isFarmExist == nil {
