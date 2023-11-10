2fd726085d5be6ee1fc8e9000b07f89b773b1a56
reyhanmichiels <michiels.reyhan@gmail.com>
2023-11-10 17:15:07 +0700

    feat(get): create route

--- rest/rest.go
+++ rest/rest.go
@@ -26,6 +26,7 @@ func (rest *Rest) HealthCheckRoute() {
 }
 
 func (rest *Rest) FarmRoute(farmHandler *farm_handler.FarmHandler) {
+	rest.engine.GET("/api/farms", farmHandler.Get)
 	rest.engine.POST("/api/farms", farmHandler.Create)
 	rest.engine.PUT("/api/farms/:farmId", farmHandler.Update)
 }
