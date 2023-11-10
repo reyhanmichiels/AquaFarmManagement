78da21534153bc25dc92c0c26391b931a18b3633
reyhanmichiels <michiels.reyhan@gmail.com>
2023-11-10 09:58:38 +0700

    refactor(createFarm): set domain Farm not return ponds

--- domain/farm.go
+++ domain/farm.go
@@ -10,7 +10,7 @@ import (
 // Model for Farm entity
 type Farm struct {
 	ID        string    `json:"id" gorm:"type:uuid; not null; primary key"`
-	Ponds     []Pond    `json:"ponds"`
+	Ponds     []Pond    `json:"-"`
 	Name      string    `json:"name" gorm:"type:varchar(100); not null; unique"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
