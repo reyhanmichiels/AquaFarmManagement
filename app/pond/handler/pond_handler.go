c67d83018db6f3f6e6ecaa144e538db5881f973b
reyhanmichiels <michiels.reyhan@gmail.com>
2023-11-11 17:10:56 +0700

    feat(updatePond): create handler

--- app/pond/handler/pond_handler.go
+++ app/pond/handler/pond_handler.go
@@ -38,3 +38,26 @@ func (pondHandler *PondHandler) Create(c *gin.Context) {
 
 	util.SuccessResponse(c, http.StatusCreated, "successfully create pond", pond)
 }
+
+func (pondHandler *PondHandler) Update(c *gin.Context) {
+	//bind request
+	var request domain.PondBind
+	err := c.ShouldBindJSON(&request)
+	if err != nil {
+		util.FailResponse(c, http.StatusBadRequest, "failed to bind request", err)
+		return
+	}
+
+	// bind param
+	pondId := c.Param("pondId")
+
+	//create pond
+	pond, errObject := pondHandler.pondUsecase.Update(request, pondId)
+	if errObject != nil {
+		errObject := errObject.(util.ErrorObject)
+		util.FailResponse(c, errObject.Code, errObject.Message, errObject.Err)
+		return
+	}
+
+	util.SuccessResponse(c, http.StatusCreated, "successfully create pond", pond)
+}
