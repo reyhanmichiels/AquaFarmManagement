271766d5796324c4eec53a5ab81064973a0bbb14
reyhanmichiels <michiels.reyhan@gmail.com>
2023-11-11 13:42:52 +0700

    test(createPond): create mock for repository

---
+++ app/pond/mock/pond_repository_mock.go
@@ -0,0 +1,30 @@
+package mock
+
+import (
+	"github.com/reyhanmichiels/AquaFarmManagement/domain"
+	"github.com/stretchr/testify/mock"
+)
+
+type PondRepositoryMock struct {
+	Mock mock.Mock
+}
+
+func (pondRepositoryMock *PondRepositoryMock) FindPondByCondition(pond any, condition string, value any) error {
+	args := pondRepositoryMock.Mock.Called(pond, condition, value)
+
+	if args[0] != nil {
+		return args[0].(error)
+	}
+
+	return nil
+}
+
+func (pondRepositoryMock *PondRepositoryMock) CreatePond(pond *domain.Pond) error {
+	args := pondRepositoryMock.Mock.Called(pond)
+
+	if args[0] != nil {
+		return args[0].(error)
+	}
+
+	return nil
+}
